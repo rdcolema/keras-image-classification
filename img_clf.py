41652fa4c46168e9ecf46cc6a6f95be35eff8826
Robert Coleman <rcoleman@fool.com>
2016-10-03 10:35:29 -0400

    Create img_clf.py

---
+++ img_clf.py
@@ -0,0 +1,292 @@
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential, model_from_json
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D, Activation, Dropout, Flatten, Dense
+from keras.callbacks import EarlyStopping
+from keras import optimizers
+from matplotlib.pyplot import imshow
+import numpy as np
+from PIL import Image
+import csv
+from scipy.misc import imresize
+import os
+import h5py
+
+
+### paths to weight files
+weights_path = '../vgg16_weights.h5'                # this is the pretrained vgg16 weights
+top_model_weights_path = '../bottleneck_model.h5'   # this is the best performing model before fine tuning
+
+
+### paths to training and testing data
+train_data_dir = '/data/train'
+validation_data_dir = '/data/validation'
+test_data_dir = '/data/test'
+
+### other hyperparameters  
+nb_train_samples = 24500
+nb_validation_samples = 500
+nb_test_samples = 12500
+nb_epoch = 25
+img_width, img_height = 200, 200
+
+# (you'll have to divide up the dataset into the right directories to match this setup
+# since the kaggle dataset doesn't come with a validation split)
+
+early_stopping = EarlyStopping(monitor='val_loss', patience=2, verbose=1, mode='auto')
+# ^^ this stops training after validation loss stops improving
+
+
+def save_bottlebeck_features():
+    """builds the pretrained vgg16 model and runs it on our training and validation datasets"""
+    datagen = ImageDataGenerator(rescale=1./255)
+
+    # match the vgg16 architecture so we can load the pretrained weights into this model
+    model = Sequential()
+    model.add(ZeroPadding2D((1, 1), input_shape=(3, img_width, img_height)))
+
+    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    # load VGG16 weights
+    f = h5py.File(weights_path)
+    
+    for k in range(f.attrs['nb_layers']):
+        if k >= len(model.layers):
+            break
+        g = f['layer_{}'.format(k)]
+        weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
+        model.layers[k].set_weights(weights)
+        
+    f.close()    
+    print 'Model loaded.'
+
+    generator = datagen.flow_from_directory(
+            train_data_dir,
+            target_size=(img_width, img_height),
+            batch_size=32,
+            class_mode=None,
+            shuffle=False)
+    bottleneck_features_train = model.predict_generator(generator, nb_train_samples)
+    np.save(open('bottleneck_features_train.npy', 'wb'), bottleneck_features_train)
+
+    generator = datagen.flow_from_directory(
+            validation_data_dir,
+            target_size=(img_width, img_height),
+            batch_size=32,
+            class_mode=None,
+            shuffle=False)
+    bottleneck_features_validation = model.predict_generator(generator, nb_validation_samples)
+    np.save(open('bottleneck_features_validation.npy', 'wb'), bottleneck_features_validation)
+
+
+def train_top_model():
+    """trains the model and adds a classifier on top"""
+    train_data = np.load(open('bottleneck_features_train.npy', 'rb'))
+    train_labels = np.array([0] * (nb_train_samples / 2) + [1] * (nb_train_samples / 2))
+
+    validation_data = np.load(open('bottleneck_features_validation.npy', 'rb'))
+    validation_labels = np.array([0] * (nb_validation_samples / 2) + [1] * (nb_validation_samples / 2))
+
+    model = Sequential()
+    model.add(Flatten(input_shape=train_data.shape[1:]))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+    model.fit(train_data, train_labels,
+              nb_epoch=nb_epoch,
+              batch_size=32,
+              validation_data=(validation_data, validation_labels),
+              callbacks=[early_stopping])
+              
+    # save the model weights
+    model.save_weights(top_model_weights_path)
+
+
+def fine_tune():
+    """recreates top model architecture/weights and fine tunes with image augmentation and optimizations"""
+    
+    # reconstruct vgg16 model
+    model = Sequential()
+    model.add(ZeroPadding2D((1, 1), input_shape=(3, img_width, img_height)))
+
+    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
+    model.add(ZeroPadding2D((1, 1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    # load vgg16 weights
+    f = h5py.File(weights_path)
+    
+    for k in range(f.attrs['nb_layers']):
+        if k >= len(model.layers):
+            break
+        g = f['layer_{}'.format(k)]
+        weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
+        model.layers[k].set_weights(weights)
+        
+    f.close()
+
+    # add the classification layers
+    top_model = Sequential()
+    top_model.add(Flatten(input_shape=model.output_shape[1:]))
+    top_model.add(Dense(256, activation='relu'))
+    top_model.add(Dropout(0.5))
+    top_model.add(Dense(1, activation='sigmoid'))
+
+    top_model.load_weights(top_model_weights_path)
+
+    # add the model on top of the convolutional base
+    model.add(top_model)
+
+    # set the first 25 layers (up to the last conv block)
+    # to non-trainable (weights will not be updated)
+    for layer in model.layers[:25]:
+        layer.trainable = False
+
+    # compile the model with a SGD/momentum optimizer
+    # and a very slow learning rate.
+    model.compile(loss='binary_crossentropy',
+                  optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
+                  metrics=['accuracy'])
+
+    # prepare data augmentation configuration
+    train_datagen = ImageDataGenerator(
+            rescale=1./255,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True)
+
+    test_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_height, img_width),
+        batch_size=32,
+        class_mode='binary')
+
+    validation_generator = test_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_height, img_width),
+        batch_size=32,
+        class_mode='binary')
+
+    # fine-tune the model
+    model.fit_generator(
+        train_generator,
+        samples_per_epoch=nb_train_samples,
+        nb_epoch=nb_epoch,
+        validation_data=validation_generator,
+        nb_val_samples=nb_validation_samples,
+        callbacks=[early_stopping])
+
+    # save the model
+    json_string = model.to_json()
+
+    with open('final_model_architecture.json', 'w') as f:
+        f.write(json_string)
+
+    model.save_weights('final_weights.h5')
+    
+    # return the model for convenience when making predictions
+    return model 
+
+
+def predict_labels(model):
+    """writes test image labels and predictions to csv"""
+    
+    test_datagen = ImageDataGenerator(rescale=1./255)
+    test_generator = test_datagen.flow_from_directory(
+        test_data_dir,
+        target_size=(img_height, img_width),
+        batch_size=32,
+        shuffle=False,
+        class_mode=None)
+
+    base_path = "../data/test/test/"
+
+    with open("prediction.csv", "w") as f:
+        p_writer = csv.writer(f, delimiter=',', lineterminator='\n')
+        for _, _, imgs in os.walk(base_path):
+            for im in imgs:
+                pic_id = im.split(".")[0]
+                img = load_img(base_path + im)
+                img = imresize(img, size=(img_height, img_width))
+                test_x = img_to_array(img).reshape(3, img_height, img_width)
+                test_x = test_x.reshape((1,) + test_x.shape)
+                test_generator = test_datagen.flow(test_x,
+                                                   batch_size=1,
+                                                   shuffle=False)
+                prediction = model.predict_generator(test_generator, 1)[0][0]
+                p_writer.writerow([pic_id, prediction])
+
+
+if __name__ == "__main__":
+    save_bottlebeck_features()
+    train_top_model()
+    model = fine_tune()
+    predict_labels(model)
